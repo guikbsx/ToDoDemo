15c8203861f0f6ee236b36b08e08d320698389ed
Guillaume Bisiaux <guillaume@heroes.jobs>
2022-05-06 09:43:47 +0200

    Initial Commit

---
+++ ToDoDemo/ToDoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDemoApp.swift
+//  ToDoDemo
+//
+//  Created by Guillaume Bisiaux on 06/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
